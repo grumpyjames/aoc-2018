c006dfb54a6538b105f23db593ba00cea84ad7e8
James Byatt <grumpyjames@gmail.com>
2019-01-05 23:18:46 +0000

    why fight with the borrow checker when you can just store secret pointers to separate vector storage?!

--- src/nine.rs
+++ src/nine.rs
@@ -1,12 +1,22 @@
 extern crate regex;
 
-use std::collections::LinkedList;
-
 #[derive(Debug)]
 struct Player
 {
     score: usize
 }
+
+struct Entry
+{
+    value: usize,
+    prev: usize,
+    next: usize,
+//    idx: usize
+}
+//
+//fn get(storage: &mut Vec<Entry>, idx: usize) -> &mut Entry {
+//    storage.get_mut(idx).unwrap()
+//}
 
 fn main() {
     let player_count = 448;
@@ -17,10 +27,10 @@ fn main() {
         players.push(Player {score: 0});
     }
 
-    let mut circle : Vec<usize> = Vec::new();
-    let mut current_index : usize = 0;
-    circle.push(0);
+    let mut storage : Vec<Entry> = Vec::with_capacity(marble_count);
 
+    storage.push(Entry { value: 0, prev: 0, next: 0 });
+    let mut current_entry_index : usize = 0;
 
     for j in 1..marble_count {
         let mut current_player = players.get_mut(j % player_count).unwrap();
@@ -28,20 +38,54 @@ fn main() {
         if j % 23 == 0
         {
             current_player.score += j;
-            current_index = if current_index >= 7 {
-                current_index - 7
-            } else {
-                circle.len() - 7 + current_index
-            };
-            let removed = circle.remove(current_index as usize);
-            current_player.score += removed;
-        }
-        else
-        {
-            current_index = (current_index + 2) % circle.len();
-            circle.insert(current_index as usize,j);
+
+            let mut skip_count = 0;
+            while skip_count < 7 {
+                current_entry_index = storage.get_mut(current_entry_index).unwrap().prev;
+                skip_count += 1;
+            }
+
+            let mut previous;
+            let mut next;
+            {
+                let mut cur = storage.get_mut(current_entry_index).unwrap();
+                previous = cur.prev;
+                next = cur.next;
+                current_player.score += cur.value;
+            }
+
+            storage.get_mut(previous).unwrap().next = next;
+            storage.get_mut(next).unwrap().prev = previous;
+            current_entry_index = next;
+
+        } else {
+            let mut skip_count = 0;
+            while skip_count < 2 {
+                current_entry_index = storage.get_mut(current_entry_index).unwrap().next;
+                skip_count += 1;
+            }
+
+            let new_index = storage.len();
+            let mut new_entry_previous;
+            {
+                let mut entry_after_new =
+                    storage.get_mut(current_entry_index).unwrap();
+                new_entry_previous = entry_after_new.prev;
+                entry_after_new.prev = new_index;
+            }
+
+            storage.push(
+                Entry {
+                    value: j,
+                    prev: new_entry_previous,
+                    next: current_entry_index
+                });
+
+            storage.get_mut(new_entry_previous).unwrap().next = new_index;
+            current_entry_index = new_index;
         }
     }
+
 
     players.sort_by(|p1, p2| p2.score.cmp(&p1.score));
     println!("{:?}", players);
